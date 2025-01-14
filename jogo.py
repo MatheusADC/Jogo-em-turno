884f1ff878477cbb13f4927e1b7b43920b805028
MatheusSuper <matheusamaralsuper@gmail.com>
2025-01-14 11:14:59 -0300

    Adc: Criação da classe Personagem

---
+++ jogo.py
@@ -0,0 +1,18 @@
+# Personagem: classe mae
+# Heroi: controlado pelo usuario 
+# Inimigo: adversario do usuario
+
+class Personagem:
+    def __init__(self, nome, vida, nivel):
+        self.__nome = nome
+        self.__vida = vida
+        self.__nivel = nivel
+
+        def get_nome(self):
+            return self.__nome
+        
+        def get_vida(self):
+            return self.__vida
+        
+        def get_nivel(self):
+            return self.__nivel
