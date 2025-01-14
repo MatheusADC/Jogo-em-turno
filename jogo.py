a68a042606b7df21bb117c7bbf2f55423bf3d30c
MatheusSuper <matheusamaralsuper@gmail.com>
2025-01-14 11:18:18 -0300

    Adc: Criação da classe Inimigo

--- jogo.py
+++ jogo.py
@@ -24,3 +24,11 @@ class Heroi(Personagem):
 
         def get_habilidade(self):
             return self.__habilidade
+        
+class Inimigo(Personagem):
+    def __init__(self, nome, vida, nivel, tipo):
+        super().__init__(nome, vida, nivel)
+        self.__tipo = tipo
+
+        def get_tipo(self):
+            return self.__tipo
